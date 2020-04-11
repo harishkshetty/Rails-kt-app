32ca3a7b4ec8a5930145d8959002fda773184984
Phyoe <phyoe@circles.asia>
2020-04-11 16:39:34 +0800

    Syntax fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,20 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources:login, only:[:show, :index,:new,:create] do
-  # get 'homeget' ,to:'login#homeget'
-  collection do
-    get :search
+  resources :login, only:[:show, :index,:new,:create] do
+    # get 'homeget' ,to:'login#homeget'
+    collection do
+      get :search
+    end
+    member do
+      get :record
+      get :add
+
+      post :update_user
+      post :create_user_order
+      post :create_user_order_with_attributes
+
+    end
+
   end
-  member do
-    get :record
-    get :add
-
-    post :update_user
-    post :create_user_order
-    post :create_user_order_with_attributes
-
-  end
-
-end
-
 end
